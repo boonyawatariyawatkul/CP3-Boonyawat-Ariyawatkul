ab52f9f4a0f21410bfae850d462e0f3a4b9f36d0
boonyawatariyawatkul <ariyawatkulb@gmail.com>
2020-04-02 18:02:08 +0700

    Added Exercise 4
    Added original exercise 4 to respository

---
+++ Exercise4_Boonyawat_A.py
@@ -0,0 +1,7 @@
+#reporting examiner's scores
+
+print ("--- Your Score ---")
+print(float(input("Foundation English :")))
+print(float(input("General Business :")))
+print(float(input("Introduction to Computer Systems :")))
+print(float(input("Computer Programming :")))
